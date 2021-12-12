6e823a62ebdd6367136a17e7692b9f9b0bfb1d50
Asam237 <abbasalimokolo@gmail.com>
2021-12-12 06:13:30 +0100

    add attribu on change text on input component

--- src/components/myInput/MyInput.component.tsx
+++ src/components/myInput/MyInput.component.tsx
@@ -13,7 +13,7 @@ export const MyInput = (props: any) => {
           borderRadius: SPACING.xsmall,
         }}
         placeholder={props.myPlaceHolder}
-        onChange={(e: any) => props.onChangeText!(e)}
+        onChangeText={(e: any) => props.onChangeText!(e)}
       />
     </View>
   );
